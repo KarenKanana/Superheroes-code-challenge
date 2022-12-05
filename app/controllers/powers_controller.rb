e531bc5c55bf342b5263a3a1d7703d797c587bb4
Karen Kanana <kananakaren21@gmail.com>
2022-12-05 14:29:37 +0300

    Create update method and private methods

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,4 +1,5 @@
 class PowersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
    # GET /powers
     def index
         @powers = Power.all
@@ -9,5 +10,22 @@ class PowersController < ApplicationController
     def show
         @power = Power.find(params[:id])
         render json: @power
+    end
+
+    def update
+        @power = Power.find(params[:id])
+        @power.update!(power_params)
+        render json: @power
+        rescue ActiveRecord::RecordInvalid => invalid
+            render json: {errors: "validation errors"}, status: :unprocessable_entity
+    end
+
+    private
+    def render_not_found_response
+        render json: { error: "Power not found" }, status: :not_found
+    end
+
+    def power_params
+        params.permit(:description)
     end
 end
